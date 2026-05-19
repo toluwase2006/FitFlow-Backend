79821d520257b5eb7eac8ac9ffa24d4528871062
toluwase2006 <08067600639te@gmail.com>
2026-05-19 10:34:10 +0100

    worked on export the route

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth.middleware');
 const { uploadAvatar } = require('../config/cloudinary');
 const { getMe, updateMe } = require('../controllers/user.controller');
-const auth = require('../middleware/auth.middleware');
-const express = require('express');  
-const router = express.Router(); 
 
 router.get('/me',  auth, getMe);
 router.put('/me',  auth, uploadAvatar.single('avatar'), updateMe);
+
+module.exports = router;
