f62eefd2266072fb4fffb79a8d8d5bc4e7ded8f7
toluwase2006 <08067600639te@gmail.com>
2026-05-19 10:30:53 +0100

    worked on express

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,8 @@
 const { uploadAvatar } = require('../config/cloudinary');
 const { getMe, updateMe } = require('../controllers/user.controller');
 const auth = require('../middleware/auth.middleware');
-const router = express.Router(); 
 const express = require('express');  
+const router = express.Router(); 
 
 router.get('/me',  auth, getMe);
 router.put('/me',  auth, uploadAvatar.single('avatar'), updateMe);
