56611761da97856789c1dbb5e69685f49dc6c98b
toluwase2006 <08067600639te@gmail.com>
2026-05-19 10:19:00 +0100

    Added router to my user.route

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,7 @@
 const { uploadAvatar } = require('../config/cloudinary');
 const { getMe, updateMe } = require('../controllers/user.controller');
 const auth = require('../middleware/auth.middleware');
+const router = express.Router(); 
 
 router.get('/me',  auth, getMe);
 router.put('/me',  auth, uploadAvatar.single('avatar'), updateMe);
